1ec6de76d967125671670179eb8ac49ab7283b64
William Coelho da Silva <39714540+Williamcs1400@users.noreply.github.com>
2021-02-12 15:56:08 -0300

    After returning

---
+++ spring-demo-oap-z-after-returning/src/com/william/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,58 @@
+package com.william.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.william.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@AfterReturning(pointcut="execution(* com.william.aopdemo.dao.AccountDAO.findAccounts(..))",
+					returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+		
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=============> Executing @AfterReturning method: " + method);
+		
+		System.out.println("\n=============> resullt is " + result);
+		
+		convertAccountNamesToUpperCase(result);
+		
+	}
+	
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		
+		for(Account temp : result) {
+			
+			String theUpperName = temp.getName().toUpperCase();
+			
+			temp.setName(theUpperName);
+		
+		}
+	}
+
+	@Before("com.william.aopdemo.aspect.WillAOPExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		System.out.println("\n=============> Executing @Before advice on addAccount()");
+		
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for(Object tempArg: args) {
+			System.out.println(tempArg);
+		}
+	}
+}
