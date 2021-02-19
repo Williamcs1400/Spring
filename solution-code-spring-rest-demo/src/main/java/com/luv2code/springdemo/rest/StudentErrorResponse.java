48c1f0f3167c64b9350dbc47593392faa8ebfdda
William Coelho da Silva <39714540+Williamcs1400@users.noreply.github.com>
2021-02-19 12:21:59 -0300

    Exception Handling - Spring Rest

---
+++ solution-code-spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentErrorResponse.java
@@ -0,0 +1,41 @@
+package com.luv2code.springdemo.rest;
+
+public class StudentErrorResponse {
+
+	private int status;
+	private String massage;
+	private long timeStamp;
+	
+	public StudentErrorResponse() {
+	}
+	
+	public StudentErrorResponse(int status, String massage, long timeStamp) {
+		this.status = status;
+		this.massage = massage;
+		this.timeStamp = timeStamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMassage() {
+		return massage;
+	}
+
+	public void setMassage(String massage) {
+		this.massage = massage;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+}
---
+++ solution-code-spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+
+<h3>Index</h3>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+<br><br>
+<a href="${pageContext.request.contextPath}/api/students">List students</a>
+</body>
+
+</html>
